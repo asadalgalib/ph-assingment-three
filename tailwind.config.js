6eac47de5a1951a2a09372dbf6f40f7ad77efb81
unknown <asadalgalib9@gmail.com>
2024-08-24 21:10:32 +0600

    break

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js}'],
-    theme: {
-        colors :{
-           
-        },
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/bg.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
