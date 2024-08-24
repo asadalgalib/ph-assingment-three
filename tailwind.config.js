a867be17aee6d118965e6658a9083cd89f187f23
unknown <asadalgalib9@gmail.com>
2024-08-25 01:29:55 +0600

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('/assets/bg.jpg')",
+        'hero-pattern': "url('./assets/bg.jpg')",
         'footer-texture': "url('/img/footer-texture.png')",
       }
     },
